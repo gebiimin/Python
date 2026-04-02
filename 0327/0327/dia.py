bf88a4036114bbb2fe87916ef6891491efb83d8a
gebiimin <143503261+gebiimin@users.noreply.github.com>
2026-04-03 00:35:29 +0900

    Add files via upload

---
+++ 0327/0327/dia.py
@@ -0,0 +1,7 @@
+n = int(input("숫자를 입력하시오: "))
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+
+for i in range(n - 1, 1 , -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
---
+++ 0327/0327/report2.py
@@ -0,0 +1,7 @@
+#예제
+
+for i in range(1, 6, 1):
+    print(i, end=" ")
+
+for i in range(10,0,-1):
+    print(i, end=" ")
